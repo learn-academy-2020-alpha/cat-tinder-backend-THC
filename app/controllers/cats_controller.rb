66d0e79e9a2ff544f014db98e62f15cf516aff9d
Heya Kwon <heyabird@gmail.com>
2020-03-30 20:56:11 -0700

    Fixed Cat Show Errors

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,23 +1,44 @@
 class CatsController < ApplicationController
     def index
-        cats = Cat.all
-        render json: cats
+        @cats = Cat.all
+        render json: @cats
     end
   
+    def show
+      @cat = Cat.find(params[:id])
+      render json: @cat
+    end
+    
     def create
         # Create a new cat
-        cat = Cat.create(cat_params)
-        if cat.valid?
-            render json: cat
+        @cat = Cat.create(cat_params)
+        if @cat.valid?
+            render json: @cat
            else
-             render json: cat.errors, status: :unprocessable_entity
+             render json: @cat.errors, status: :unprocessable_entity
            end
     end
 
-    def show
-      cat = Cat.find([params[:id]])
-      render json: cat
-    end
+    def update
+      @cat = Cat.find(params[:id])
+      @cat.update(cat_params)
+      if @cat.valid?
+          render json: @cat
+      else 
+          render json: @cat.errors
+      end
+  end
+
+  def destroy
+      @cat = Cat.find(params[:id])
+      if @cat.destroy
+          render json: @cat
+      else
+          render json: @cat.errors
+      end
+  end
+
+
     
     # Handle strong parameters, so we are secure
     private
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,4 @@
 class Cat < ApplicationRecord
     validates :name, :age, :hobby, presence: true
-    validates :hobby, length: { minimum: 10 }
+    validates_length_of :hobby, minimum: 10
 end
